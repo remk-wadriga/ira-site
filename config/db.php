2f02dc4c6b93689d05df7178068f64ec53f88bc9
Rem Kwadriga <remkwadriga@yandex.ua>
2016-03-23 00:20:24 +0200

    New db config

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ira_site',
-    'username' => 'web',
-    'password' => 'web',
+    'dsn' => 'mysql:host=localhost;dbname=ac001_db',
+    'username' => 'ac001_web',
+    'password' => 'okljuhytHsdjf',
     'charset' => 'utf8',
 ];
