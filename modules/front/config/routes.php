c823a3c317e61de0f950788d71710e88f3a017db
Rem Kwadriga <remkwadriga@yandex.ua>
2016-06-14 03:48:56 +0300

    Cpu urls for events and posts

--- modules/front/config/routes.php
+++ modules/front/config/routes.php
@@ -1,9 +1,11 @@
 <?php
 
 return [
+    // Event
     'events'                                                          => 'front/event/list',
     'event-<id\d+>/register'                                          => 'front/event/register',
     'event-<id(.+)>'                                                  => 'front/event/view',
 
+    // Post
     'post-<id(.+)>'                                                   => 'front/post/view',
 ];
